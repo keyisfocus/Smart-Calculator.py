47ffd814fccf9fedd7a79afac9f549085bbd657e
keyisfocus <keyisfocus@gmail.com>
2021-05-16 13:39:05 +0300

    did_something

--- calculator.py
+++ calculator.py
@@ -1,2 +1,8 @@
-nums = [int(i) for i in input().split()]
-print(sum(nums))
+while True:
+    line = input().lower()
+    if line == '/exit':
+        print('Bye!')
+        break
+    if line:
+        nums = [int(i) for i in line.split()]
+        print(sum(nums))
