6824b7816bf223805b62216762c78961acde221b
keyisfocus <keyisfocus@gmail.com>
2021-05-16 13:41:57 +0300

    did_something

--- calculator.py
+++ calculator.py
@@ -3,6 +3,9 @@ while True:
     if line == '/exit':
         print('Bye!')
         break
+    if line == '/help':
+        print('The program calculates the sum of numbers')
+        continue
     if line:
         nums = [int(i) for i in line.split()]
         print(sum(nums))
